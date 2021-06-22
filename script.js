0be8682a0698487392a4809105a919741d8f6e20
wiznick <wiznick79@yahoo.gr>
2021-06-23 00:11:49 +0100

    nada

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 import prettyBytes from 'pretty-bytes'
 import setupEditors from './editor'
